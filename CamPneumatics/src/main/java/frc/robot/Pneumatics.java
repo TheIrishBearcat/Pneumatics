8172531f7d59294f3e2ba096e3ab18fdddd43b82
camclarke64 <camsyta81600@gmail.com>
2019-01-14 17:31:37 -0600

    Added a compressor

--- CamPneumatics/src/main/java/frc/robot/Pneumatics.java
+++ CamPneumatics/src/main/java/frc/robot/Pneumatics.java
@@ -1,12 +1,15 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Compressor;
 
 public class Pneumatics {
 
     DoubleSolenoid piston1;
+    Compressor mCompressor;
 
     public Pneumatics() {
         piston1 = new DoubleSolenoid(0, 1, 0);
+        mCompressor = new Compressor(0);
     }
 }
