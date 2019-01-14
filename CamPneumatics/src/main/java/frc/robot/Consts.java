719a0e4cd3975c28018fc0db34b3b3cc94507722
camclarke64 <camsyta81600@gmail.com>
2019-01-14 17:58:20 -0600

    Defined some constants, added some basic methods to pneumatics

---
+++ CamPneumatics/src/main/java/frc/robot/Consts.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+public class Consts {
+
+    public static final int xBoxPort = 0;
+    public static final int compressorPort = 0;
+}
--- CamPneumatics/src/main/java/frc/robot/Pneumatics.java
+++ CamPneumatics/src/main/java/frc/robot/Pneumatics.java
@@ -1,15 +1,36 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Compressor;
 
 public class Pneumatics {
 
-    DoubleSolenoid piston1;
+    DoubleSolenoid kickOut, takeIn;
     Compressor mCompressor;
+    XboxController xBox;
 
     public Pneumatics() {
-        piston1 = new DoubleSolenoid(0, 1, 0);
-        mCompressor = new Compressor(0);
+        kickOut = new DoubleSolenoid(1, 2, 3);
+        mCompressor = new Compressor(Consts.compressorPort);
+        xBox = new XboxController(Consts.xBoxPort);
+    }
+
+    public void ballKickOut() {
+        kickOut.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public void ballTakeIn() {
+        takeIn.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void xBoxButtons() {
+        if (xBox.getAButton()) {
+            ballKickOut();
+        }
+
+        if (xBox.getBButton()) {
+            ballTakeIn();
+        }
     }
 }
