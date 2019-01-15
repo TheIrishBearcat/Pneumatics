eedf384efa350be91848c0eafce5117adc187323
camclarke64 <camsyta81600@gmail.com>
2019-01-15 10:55:51 -0600

    Defined more constants

--- CamPneumatics/src/main/java/frc/robot/Consts.java
+++ CamPneumatics/src/main/java/frc/robot/Consts.java
@@ -5,4 +5,7 @@ public class Consts {
     public static final int xBoxPort = 0;
     public static final int compressorPort = 0;
     public static final int pressureLevelAnalogPin = 0;
+    public static final int millisecondsPerSecond = 1000;
+	public static final int timeOutMs = 10;
+	public static final double wheelRadiusInch = 3;
 }
