c3640c3a321610e4a2956db5ee6001039ae54193
camclarke64 <camsyta81600@gmail.com>
2019-01-15 16:23:19 -0600

    Added some more constants

--- CamPneumatics/src/main/java/frc/robot/Consts.java
+++ CamPneumatics/src/main/java/frc/robot/Consts.java
@@ -7,5 +7,6 @@ public class Consts {
     public static final int pressureLevelAnalogPin = 0;
     public static final int millisecondsPerSecond = 1000;
 	public static final int timeOutMs = 10;
-	public static final double wheelRadiusInch = 3;
+    public static final int wheelRadiusInch = 3;
+    public static final int digitalInputPort = 3;
 }
