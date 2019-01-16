96232a4509c4d67fddbb4ba601c3d0b062dba14a
camclarke64 <camsyta81600@gmail.com>
2019-01-16 16:03:25 -0600

    Added basic code for DriveTrain

--- CamPneumatics/src/main/java/frc/robot/DriveTrain.java
+++ CamPneumatics/src/main/java/frc/robot/DriveTrain.java
@@ -1,5 +1,21 @@
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.SPI;
+
 public class DriveTrain {
-    
+
+    Pneumatics pCode = new Pneumatics();
+    PowerDistributionPanel pdp = new PowerDistributionPanel();
+    AHRS navx;
+
+    int currentCase;
+    double rotateToAngleRate;
+
+    public DriveTrain(Pneumatics piston) {
+        navx = new AHRS(SPI.Port.kMXP);
+        pCode = piston;
+    }
 }
