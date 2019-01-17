849a058afa49a31def1ac6d22120671107996aef
camclarke64 <camsyta81600@gmail.com>
2019-01-17 12:05:20 -0600

    Deleted PID code

--- CamPneumatics/src/main/java/frc/robot/DriveTrain.java
+++ CamPneumatics/src/main/java/frc/robot/DriveTrain.java
@@ -1,8 +1,6 @@
 package frc.robot;
 
 import com.kauailabs.navx.frc.AHRS;
-
-import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.SPI;
 
@@ -18,10 +16,5 @@ public class DriveTrain {
     public DriveTrain(Pneumatics piston) {
         navx = new AHRS(SPI.Port.kMXP);
         pCode = piston;
-        navx.setPIDSourceType(PIDSourceType.kDisplacement);
-    }
-
-    public PIDSourceType getPidSourceType() {
-        return PIDSourceType.kDisplacement;
     }
 }
