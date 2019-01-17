0469efbf1196fbd658242a82fef9827159078ed6
camclarke64 <camsyta81600@gmail.com>
2019-01-17 10:02:04 -0600

    Added some PID code for NavX

--- CamPneumatics/src/main/java/frc/robot/DriveTrain.java
+++ CamPneumatics/src/main/java/frc/robot/DriveTrain.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.SPI;
 
@@ -17,5 +18,14 @@ public class DriveTrain {
     public DriveTrain(Pneumatics piston) {
         navx = new AHRS(SPI.Port.kMXP);
         pCode = piston;
+        navx.setPIDSourceType(PIDSourceType.kDisplacement);
+    }
+
+    public PIDSourceType getPidSourceType() {
+        return PIDSourceType.kDisplacement;
+    }
+
+    public void setPidSourceType(PIDSourceType pidSource) {
+
     }
 }
